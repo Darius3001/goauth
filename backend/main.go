f5366cabc4527821bbedd71692c6cb8c18821235
darius <darius.mertin@outlook.com>
2023-09-18 20:23:38 +0200

    bound in database

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"database/sql"
 )
 
 func main() {
 
+	err := executeQuery(func(db *sql.DB) error {
+		
+		rows, err := db.Query("SHOW TABLES")
+
+		if err != nil {
+			return err
+		}			
+		defer rows.Close()
+
+		fmt.Println("Tables in Database")
+		for rows.Next() {
+			var tableName string
+			if err := rows.Scan(&tableName); err != nil {
+				return err
+			}
+			fmt.Println(tableName)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println()
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 
 		responseString := "HIIIIII"
@@ -19,6 +46,8 @@ func main() {
 			return
 		}
 	})
+
+	http.Handle("/auth", createAuthRoute())	
 
 	http.ListenAndServe(":8080", nil)
 }
