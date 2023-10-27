4caa7525e3d5e2bd6c87dccb72036495ac98ffe3
darius <darius.mertin@outlook.com>
2023-10-27 11:50:48 +0200

    implemented jwt functions

--- backend/userauth/jwtsessions.go
+++ backend/userauth/jwtsessions.go
@@ -1,15 +1,14 @@
 package userauth
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"log"
 	"time"
-
-	"github.com/dgrijalva/jwt-go"
 )
 
-var jwt_secret []byte = []byte("myunsafesecret")
+var jwt_secret = []byte("myunsafesecret")
 
-func generateToken(userId int) string {
+func GenerateToken(userId int) string {
 
 	validityDuration := time.Hour * 4
 
@@ -27,4 +26,30 @@ func generateToken(userId int) string {
 	}
 
 	return signedToken
+}
+
+func GetUserIdAndValidateToken(tokenString string) (string, error) {
+
+	claims, err := validateSignature(tokenString)
+	if err != nil {
+		return "", err
+	}
+	return claims["userId"].(string), nil
+}
+
+func validateSignature(tokenString string) (jwt.MapClaims, error) {
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwt_secret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, jwt.ErrSignatureInvalid
 }
